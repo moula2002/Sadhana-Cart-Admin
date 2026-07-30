65b585492cd4a5c0dd6fd9e5592b06da7d452ef1
moula2002 <iammoulahussain@gmail.com>
2026-07-30 18:21:44 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ const App = () => {
 
                 <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
 
-                <div className="flex-1 lg:ml-64 flex flex-col min-h-screen">
+                <div className="flex-1 lg:ml-64 flex flex-col min-h-screen min-w-0 overflow-x-hidden w-full">
 
                   <Header
                     onToggleSidebar={toggleSidebar}
